3cc46b4535afd6d36f2d49bc4b7dfdd6bd5a01a2
Eri-ella <manuellaakoue0@gmail.com>
2026-08-10 16:42:39 +0100

    add tailwind

---
+++ resources/views/layout/client_layout.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+    @vite(['public/js/client.js'])
+</head>
+<body>
+    <header class='flex justify-between bg-(--black_color)'>
+        <a href=""><p>audiophile</p></a>
+        <ul class='flex'>
+            <a href="#">
+                <li>home</li>
+            </a>
+            <a href="#">
+                <li>headphones</li>
+            </a>
+            <a href="#">
+                <li>speakers</li>
+            </a>
+            <a href="#">
+                <li>earphones</li>
+            </a>    
+        </ul>
+        <a href="#"><iconify-icon icon="mdi-light:cart"></iconify-icon></a>
+    </header>
+
+    <footer>
+        <div>
+            <span></span>
+            <a href=""><p>audiophile</p></a>
+            <p>Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we're open 7 days a week.</p>
+            <p>Copyright 2021. All Rights Reserved</p>
+        </div>
+        <div>
+            <ul>
+                <a href="#">
+                    <li>home</li>
+                </a>
+                <a href="#">
+                    <li>headphones</li>
+                </a>
+                <a href="#">
+                    <li>speakers</li>
+                </a>
+                <a href="#">
+                    <li>earphones</li>
+                </a>    
+            </ul>
+            <div>
+                <a href="#"><iconify-icon icon="uil:facebook"></iconify-icon></a>
+                <a href="#"><iconify-icon icon="mdi:twitter"></iconify-icon></a>
+                <a href="#"><iconify-icon icon="mdi:instagram"></iconify-icon></a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
